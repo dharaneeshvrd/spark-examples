bd3792f212263d1fc6fd0777b327a8f66cf6464e
Dharaneeshwaran <dharanish185@gmail.com>
2018-10-11 04:32:13 +0000

    Data formatting - date added

--- python/med_analysis.py
+++ python/med_analysis.py
@@ -2,6 +2,7 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
 from pyspark.sql.types import *
 import csv
+import datetime
 import time
 
 conf = SparkConf().setAppName('test_med_analysis')
@@ -15,6 +16,7 @@ fields = [StructField(ele, StringType(), True) for ele in header.split(',')]
 fields[3].dataType = IntegerType()
 fields[5].dataType = IntegerType()
 fields[30].dataType = FloatType()
+fields[31].dataType = TimestampType()
 fields[32].dataType = IntegerType()
 
 schema = StructType(fields)
@@ -23,23 +25,26 @@ del_header = gpay.filter(lambda l: 'Change_Type' in l)
 new_gpay = gpay.subtract(del_header)
 
 def split_data(istr):
-	l = []
-	for ele in csv.reader(istr.split(',')):
-		if not ele:
-			ele = ['']
-		l += ele
-	return l
+    l = []
+    for ele in csv.reader(istr.split(',')):
+	if not ele:
+            ele = ['']
+        l += ele
+    return l
 	
 def create_tuple(il):
-	jl = ()
-	for i, e in enumerate(il):
-		if i in [3,5,32] and e:
-			jl += (int(e.strip('"')),)
-		elif i in [30] and e:
-			jl += (float(e.strip('"')),)
-		else:
-			jl += (e,)
-	return jl
+     jl = ()
+     for i, e in enumerate(il):
+          if i in [3,5,32] and e:
+              jl += (int(e.strip('"')),)
+          elif i in [30] and e:
+              jl += (float(e.strip('"')),)
+          elif i in [31] and e:
+              tl = e.strip('"').split('/')
+              jl += (datetime.datetime(int(tl[2]),int(tl[0]),int(tl[1])),)
+          else:
+              jl += (e,)
+     return jl
 
 	
 final_gpay = new_gpay.map(split_data).map(create_tuple)
