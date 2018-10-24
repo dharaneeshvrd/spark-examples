3e477f6173ac29ee57349938fccde69ab4820045
Dharaneeshwaran <dharanish185@gmail.com>
2018-10-24 07:40:40 -0400

    rsrchpay 2013 to 2016's ds from plain csv to parquet conversion added

--- python/spark2.3/rsrch_pay/query_exec.py
+++ python/spark2.3/rsrch_pay/query_exec.py
@@ -3,14 +3,17 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 
-conf = SparkConf().setAppName('RPAY_DS_QUERIES')
-sc = SparkContext(conf=conf)
 spark = SparkSession \
     .builder \
     .appName("RPAY_DS_QUERIES") \
     .getOrCreate()
 
-rpay_df = spark.read.parquet('/tmp/spark_poc/parq_out_rsrch')
+rpay_df = spark.read.option('mergeSchema', 'true').parquet('/tmp/spark_poc/parq_out_rsrch_2013',
+                                                            '/tmp/spark_poc/parq_out_rsrch_2014',
+                                                            '/tmp/spark_poc/parq_out_rsrch_2015',
+                                                            '/tmp/spark_poc/parq_out_rsrch_2016',
+                                                            '/tmp/spark_poc/parq_out_rsrch_2017')
+
 grouping_cols = ['Teaching_Hospital_Name',
                  'Principal_Investigator_1_Specialty',
                  'Principal_Investigator_2_Specialty',
